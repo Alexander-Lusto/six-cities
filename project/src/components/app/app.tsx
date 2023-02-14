38ae278bee9e654df366bce18c3f84769e7820ba
Alexander Lusto <you@example.com>
2023-02-14 11:51:41 +0300

    Разбил разметку на  компоненты и отрендерил их на странице

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,22 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+import Header from '../header/header';
+import Page from '../page/page';
+
+type Props = {
+  cardsNumber: number;
+};
+
+function App(props: Props): JSX.Element {
+  const { cardsNumber } = props;
+
+  return (
+    <Page>
+      <>
+        <Header></Header>
+        <Main cardsNumber={cardsNumber} />
+      </>
+    </Page>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const CARDS_NUMBER = 5;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App cardsNumber={CARDS_NUMBER} />
   </React.StrictMode>,
 );
