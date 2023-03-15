cfe428a2610a8c01ef5a9b7a1c735af48e89cd55
M1ster Rob0t <you@example.com>
2023-03-15 15:03:12 +0300

    Написал тесты для редьюсера propertyData

--- project/src/store/authorization-process/authorization-process.test.ts
+++ project/src/store/authorization-process/authorization-process.test.ts
@@ -1,21 +1,22 @@
 import { AuthorizationStatus } from '../../const';
 import { requireAuth, requireLogout } from '../action';
 import { authorizationProcess } from './authorization-process';
+import { TAuthorizationState } from '../../types/state';
 
 describe('Reducer: authorization-process', () => {
 
   test('should set authStatus in state to "Auth"', () => {
-    const state = {authStatus: AuthorizationStatus.Unknown};
+    const state: TAuthorizationState = {authStatus: AuthorizationStatus.Unknown};
     expect(authorizationProcess(state, requireAuth(AuthorizationStatus.Auth))).toEqual({authStatus: AuthorizationStatus.Auth});
   });
 
   test('should set authStatus in state to "NoAuth"', () => {
-    const state = {authStatus: AuthorizationStatus.Unknown};
+    const state: TAuthorizationState = {authStatus: AuthorizationStatus.Unknown};
     expect(authorizationProcess(state, requireAuth(AuthorizationStatus.NoAuth))).toEqual({authStatus: AuthorizationStatus.NoAuth});
   });
 
   test('should change authStatus in state from "Auth" to "NoAuth"', () => {
-    const state = {authStatus: AuthorizationStatus.Auth};
+    const state: TAuthorizationState = {authStatus: AuthorizationStatus.Auth};
     expect(authorizationProcess(state, requireLogout())).toEqual({authStatus: AuthorizationStatus.NoAuth});
   });
 });
---
+++ project/src/store/property-data/property-data.test.ts
@@ -0,0 +1,27 @@
+import { TPropertyState } from '../../types/state';
+import { setComments, setOfferData, setOffer } from '../action';
+import { propertyData } from './property-data';
+import { mockComments } from '../../mock/commetns';
+import { mockOffers } from '../../mock/offers';
+import { TOfferData } from '../../types/offerData';
+
+
+describe('Reducer propertyData', () => {
+  test('should set offer in state', () => {
+    const state: TPropertyState = { offer: null, offersNearby: null, comments: null };
+    expect(propertyData(state, setOffer(mockOffers[0]))).toEqual({ offer: mockOffers[0], offersNearby: null, comments: null });
+  });
+
+  test('should set comments in state', () => {
+    const state: TPropertyState = { offer: mockOffers[2], offersNearby: null, comments: null };
+    expect(propertyData(state, setComments(mockComments[0]))).toEqual({ offer: mockOffers[2], offersNearby: null, comments: mockComments[0] });
+  });
+
+  test('should set offer, comments, offersNearby in state', () => {
+    const state: TPropertyState = { offer: null, offersNearby: null, comments: null };
+    const mockOffersNearby = [mockOffers[1], mockOffers[2], mockOffers[3]];
+    const mockOfferData: TOfferData = {offer: mockOffers[0], offersNearby: mockOffersNearby, comments: mockComments[0]};
+
+    expect(propertyData(state, setOfferData(mockOfferData))).toEqual({offer: mockOffers[0], offersNearby: mockOffersNearby,comments: mockComments[0]});
+  });
+});
